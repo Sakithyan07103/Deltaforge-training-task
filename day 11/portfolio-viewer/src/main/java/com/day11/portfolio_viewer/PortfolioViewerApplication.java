f628461794787012b0aeb910462868df2f60cb75
Sakithyan07103 <Sakithyan.Ilamaran@deltaforge.tech>
2025-07-21 14:40:24 +0530

    Add files via upload

---
+++ day 11/portfolio-viewer/src/main/java/com/day11/portfolio_viewer/PortfolioViewerApplication.java
@@ -0,0 +1,13 @@
+package com.day11.portfolio_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioViewerApplication.class, args);
+	}
+
+}
