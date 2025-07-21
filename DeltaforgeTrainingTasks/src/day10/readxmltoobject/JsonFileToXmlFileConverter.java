881d7c138d4b995031448cfa974f94fa79fd6527
Sakithyan Ilamaran <Sakithyan.Ilamaran@deltaforge.tech>
2025-07-21 10:52:46 +0530

    completed weekend 2 task

--- DeltaforgeTrainingTasks/src/day10/readxmltoobject/JsonFileToXmlFileConverter.java
+++ DeltaforgeTrainingTasks/src/day10/readxmltoobject/JsonFileToXmlFileConverter.java
@@ -10,23 +10,22 @@ import java.io.FileWriter;
 public class JsonFileToXmlFileConverter {
     public static void main(String[] args) throws IOException, ParseException {
         try {
-            // Read JSON file
             FileReader reader = new FileReader("day9.json");
             JSONParser parser = new JSONParser();
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
-            // Convert to XML manually (basic version)
             StringBuilder xmlBuilder = new StringBuilder();
             xmlBuilder.append("<root>\n");
+
             for (Object key : jsonObject.keySet()) {
                 Object value = jsonObject.get(key);
                 xmlBuilder.append("  <").append(key).append(">")
                         .append(value)
                         .append("</").append(key).append(">\n");
             }
-            xmlBuilder.append("</root>");
 
-            // Write XML to file
+            xmlBuilder.append("</root>\n");
+
             FileWriter writer = new FileWriter("output.xml");
             writer.write(xmlBuilder.toString());
             writer.close();
