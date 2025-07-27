5079eb360f0bbf9dc88ec026a14e318735591ef4
Sakithyan Ilamaran <Sakithyan.Ilamaran@deltaforge.tech>
2025-07-27 22:38:19 +0530

    completed day 15 task

--- day15/students-mark-portal/src/main/java/com/day15/students_mark_portal/service/serviceinterface/StudentService.java
+++ day15/students-mark-portal/src/main/java/com/day15/students_mark_portal/service/serviceinterface/StudentService.java
@@ -1,10 +1,11 @@
 package com.day15.students_mark_portal.service.serviceinterface;
 
 import com.day15.students_mark_portal.model.Students;
-
+import org.mapstruct.Mapper;
 import java.util.List;
 import java.util.Optional;
 
+@Mapper(componentModel = "spring")
 public interface StudentService {
     public Students createStudent(String name, int roll);
 
