9214be49c66b7290ecca5c04dc7e0b3d613b11d2
Sakithyan Ilamaran <Sakithyan.Ilamaran@deltaforge.tech>
2025-07-27 22:40:30 +0530

    quick update

--- day15/students-mark-portal/src/main/java/com/day15/students_mark_portal/mapper/MarksMapper.java
+++ day15/students-mark-portal/src/main/java/com/day15/students_mark_portal/mapper/MarksMapper.java
@@ -3,7 +3,6 @@ package com.day15.students_mark_portal.mapper;
 import com.day15.students_mark_portal.dto.MarksDTO;
 import com.day15.students_mark_portal.model.Marks;
 import org.mapstruct.Mapper;
-
 import java.util.List;
 
 @Mapper(componentModel = "spring")
